7744b2da116ad804a6d00ab8f3e25702c298fea2
Niklas Svantesson <svantesson.niklas@gmail.com>
2024-03-31 13:58:41 +0200

    Restart part 6

--- game_logic.py
+++ game_logic.py
@@ -33,35 +33,53 @@ def Run():
         if each.type == pygame.QUIT:
             return False
 
-    global current_phase
     global last_phase
+    global current_phase   
+    global level_pods
+    global enemies_present
+
     if current_phase != last_phase:
         print (f"Phase changed from {last_phase} to {current_phase}") # add logic for when phase changes
         last_phase = current_phase
     
     match current_phase:
-        case GamePhase.Exploring: Exploring()
-        case GamePhase.Combat: Combat()
-        case GamePhase.GameOver: GameOver()
+        case GamePhase.Exploring: Exploring(current_phase, level_pods, enemies_present)
+        case GamePhase.Combat: Combat(current_phase, level_pods, enemies_present)
+        case GamePhase.GameOver:
+            GameOver(level_pods, enemies_present)
+            return False
         case _:
             print (f"Invalid phase {current_phase}")
             current_phase = GamePhase.Exploring  
 
     return True
 
-def Exploring():
+def Exploring(phase : GamePhase, level_pods : set, enemies_present : dict):
     # check for enemies, if not empty switch to combat, else pop from level_pods
     # if level_pods is empty switch to game over (Victory)
-    pass
+    if enemies_present:
+        phase = GamePhase.Combat
+    elif level_pods:
+        match level_pods.pop():
+            case EnemyType.OrenianSwordsmen: pass # add 1-3 orenian swordsmen
+            case EnemyType.OrenianHammerman: pass # add 1 orenian hammerman
+            case _: print ("Invalid enemy type")
+        phase = GamePhase.Combat
+    else:
+        current_phase = GamePhase.GameOver
+        print ("Victory!")
 
-def Combat():
+def Combat(phase : GamePhase, level_pods : set, enemies_present : dict):
     # if player dies switch to game over (Defeat)
-    # if player kills all enemies switch to exploring
-    pass
+    if not enemies_present:
+        current_phase = GamePhase.Exploring
 
-def GameOver():
-    # take argument to determine win or lose
+    # combat here
+
+def GameOver(level_pods : set, enemies_present : dict):
+    if enemies_present or level_pods:
+        print ("Defeat!")
+    else:
+        print ("Victory!")
     # end game on player confirmation (input)
     # highscore?
-    pass
-
