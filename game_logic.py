f296ea4df6db39bc7a44e9020a70b8ab2a51bc22
Niklas Svantesson <svantesson.niklas@gmail.com>
2024-03-31 13:24:28 +0200

    Restart part 2

--- game_logic.py
+++ game_logic.py
@@ -1,1 +1,11 @@
 # SummonRee - game_logic.py
+import pygame
+
+def Run():
+    for each in pygame.event.get():
+        if each.type == pygame.QUIT:
+            return False
+        
+    
+
+    return True
--- main.py
+++ main.py
@@ -1,1 +1,23 @@
 # SummonRee - main.py
+
+import pygame
+from pygame.time import Clock
+
+from game_logic import Run
+
+pygame.init()
+clock = Clock()
+
+# create window
+window = pygame.display.set_mode((640, 480))
+
+
+while Run():
+    # make basic window
+    window.fill((100, 100, 100))    
+    # draw
+    pygame.display.update()
+    
+    clock.tick(60)
+
+pygame.quit()
