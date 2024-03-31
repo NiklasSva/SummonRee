ec8666ead20253100a156eef5813ab36694089f7
Niklas Svantesson <svantesson.niklas@gmail.com>
2024-03-31 14:21:37 +0200

    Restart part 9

--- gameobjects/base_entity.py
+++ gameobjects/base_entity.py
@@ -6,3 +6,4 @@ import uuid
 def __init__(self):
     self.position = pygame.Vector2(0, 0)
     self.ID = uuid.uuid4()
+    self.name = "UnknownEntity" + str(self.ID)
--- gameobjects/player_character.py
+++ gameobjects/player_character.py
@@ -1,1 +1,11 @@
 # SummonRee - player_character.py
+
+import base_entity
+import pygame
+class PlayerCharacter(base_entity.BaseEntity):
+    def __init__(self, name:str , position2D:pygame.Vector2, speed:float = 100.0):
+        super().__init__()
+        self.name = name             
+        if position2D: self.position = position2D
+        self.speed = speed
+        
